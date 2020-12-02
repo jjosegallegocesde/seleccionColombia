44fcb85fae04acd2e95ec965993bffcf48dbc2d8
Juan <juan.gallegomesa@gmail.com>
2020-12-02 16:44:13 -0500

    Initial commit

---
+++ servicios.js
@@ -0,0 +1,12 @@
+let boton = document.getElementById("boton");
+
+boton.addEventListener('click', agregarJugador);
+
+function agregarJugador() {
+
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+    let posicion = document.getElementById("posicion").value;
+    let equipo = document.getElementById("equipo").value;
+
+}
